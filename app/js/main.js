a71c190317167b3578a07eddbad8925a7e37612a
LiJiaWei <525469647@qq.com>
2019-01-20 10:51:14 +0800

    加标题,路由懒加载

--- app/js/main.js
+++ app/js/main.js
@@ -2,9 +2,17 @@ import Vue from "vue"
 import App from "./App.vue"
 import router from "./router"
 import VueAwesomeSwiper from 'vue-awesome-swiper'
+import { setTitle } from './tools/util'
+
 
 Vue.use(VueAwesomeSwiper)
 Vue.config.productionTip = false
+
+
+router.beforeEach((to, from, next) => {
+    to.meta && setTitle(to.meta.title)
+    next()
+})
 
 /* eslint-disable no-new */
 new Vue({
--- app/js/router/index.js
+++ app/js/router/index.js
@@ -2,14 +2,7 @@ import Vue from "vue"
 import Router from "vue-router"
 //首页
 import Home from "../home/index.vue"
-//理财
-import Money from "../money/index.vue"
-// 白条
-import Ious from "../ious/index.vue"
-// 众筹
-import Raise from "../raise/index.vue"
-//下载页
-import DownLoad from '../special/download.vue'
+
 import "../../css/reset.scss"
 Vue.use(Router)
 
@@ -20,25 +13,37 @@ export default new Router({
             name: "home",
             component: Home,
         },
-        {
+        {   //理财
             path: "/Money",
             name: "money",
-            component: Money,
+            component: () => import('../money'),
+            meta: {
+                title: '理财'
+            }
         },
-        {
+        {  // 白条
             path: "/Ious",
             name: "ious",
-            component: Ious,
+            component: () => import("../ious"),
+            meta: {
+                title: '白条'
+            }
         },
-        {
+        {   //众筹
             path: "/Raise",
             name: "raise",
-            component: Raise,
+            component: () => import('../raise'),
+            meta:{
+                title:'众筹'
+            }
         },
-        {
-            path:'/Download',
-            name:'download',
-            component: DownLoad
+        {  //下载页
+            path: '/Download',
+            name: 'download',
+            component: () => import('../special/download.vue'),
+            meta:{
+                title:'下载APP'
+            }
         }
     ],
 })
